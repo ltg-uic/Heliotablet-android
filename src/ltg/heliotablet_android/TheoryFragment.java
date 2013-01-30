7bdeb524eb7267d48801b180df80e30e8ad1dd88
Anthony Perritano <aperritano@gmail.com>
2013-01-29 22:38:28 -0600

    refactor

--- src/ltg/heliotablet_android/TheoryFragment.java
+++ src/ltg/heliotablet_android/TheoryFragment.java
@@ -1,17 +1,16 @@
 package ltg.heliotablet_android;
 
+import ltg.heliotablet_android.view.CircleView;
+import ltg.heliotablet_android.view.PopoverView;
+import ltg.heliotablet_android.view.PopoverView.PopoverViewDelegate;
 import android.content.ClipData;
-import android.content.ClipDescription;
-import android.graphics.Canvas;
-import android.graphics.Color;
 import android.graphics.Point;
-import android.graphics.drawable.ColorDrawable;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.util.Log;
 import android.view.DragEvent;
-import android.view.GestureDetector;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
@@ -20,124 +19,223 @@ import android.view.View.OnDragListener;
 import android.view.View.OnTouchListener;
 import android.view.ViewGroup;
 import android.view.ViewGroup.LayoutParams;
+import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
-
 public class TheoryFragment extends Fragment {
+	
+	private View mainLayoutView;
+	private View slideScreen;
+	private RelativeLayout viewPagerLayout;
+
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 
-		View view = inflater.inflate(R.layout.theories_activity_vertical,
-				container, false);
-		View earthView = view.findViewById(R.id.earthView);
-		// Draggable colored circles
-		FrameLayout planetColorsView = (FrameLayout) view
-				.findViewById(R.id.colors_include);
 
-		earthView.setOnDragListener(new MyDragListener());
-		// earthView.setOnTouchListener(new MyTouchListener());
+		mainLayoutView = inflater.inflate(R.layout.theories_activity_vertical, container, false);
+		viewPagerLayout = (RelativeLayout) inflater.inflate(R.layout.activity_screen_slide, container, false);
+		View earthView = mainLayoutView.findViewById(R.id.earthView);
+		View neptuneView = mainLayoutView.findViewById(R.id.neptuneView);
+		View mercuryView = mainLayoutView.findViewById(R.id.mercuryView);
+		//Draggable colored circles
+		FrameLayout planetColorsView = (FrameLayout) mainLayoutView.findViewById(R.id.colors_include);
+		
+		
+		
+		
+		Button button = (Button) earthView.findViewById(R.id.testButton);
+		button.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Log.i("onClickButton", "test button");
 
-		// get all the colors and the
-		int childCount = planetColorsView.getChildCount();
-		for (int i = 0; i < childCount; i++) {
-			View color = planetColorsView.getChildAt(i);
-			color.setOnTouchListener(new MyTouchListener());
-			color.setOnDragListener(new MyDragListener());
-			System.out.println(color);
-			// do whatever you want to with the view
-		}
+				
+				ViewPager pager = new ViewPager(TheoryFragment.this.getActivity());
+				
+				ViewPager vPager = (ViewPager) viewPagerLayout.findViewById(R.id.pager);
+				
+				vPager.setAdapter(new PagerAdapter() {
 
-		return view;
-	}
+			        @Override
+			        public boolean isViewFromObject(View arg0, Object arg1) {
+			            return arg0 == arg1;
+			        }
 
-	private final class MyTouchListener implements OnTouchListener {
-		public boolean onTouch(View view, MotionEvent event) {
+			        @Override
+			        public int getCount() {
+			            return 2;
+			        }
 
-			switch (event.getAction()) {
-			case MotionEvent.ACTION_DOWN:
-				ClipData data = ClipData.newPlainText("", "");
-				DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(
-						view);
-				view.startDrag(data, shadowBuilder, view, 0);
-				view.setVisibility(View.INVISIBLE);
-				break;
+			        /* (non-Javadoc)
+			         * @see android.support.v4.view.PagerAdapter#instantiateItem(android.view.ViewGroup, int)
+			         */
+			        @Override
+			        public Object instantiateItem(ViewGroup container, int position) {
+			        	
+			        	if( position == 0 ) {
+			            View view = View.inflate(container.getContext(), R.layout.popover_view, null);
+			            container.addView(view, LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT);
+			            return view;
+			        	} else {
+			        		View view = View.inflate(container.getContext(), R.layout.fragment_screen_slide_page, null);
+				            container.addView(view, LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT);
+				            return view;
+			        	}
+			        }
 
-			case MotionEvent.ACTION_UP:
-				view.setVisibility(View.VISIBLE);
-				break;
-			}
-			return true;
-		}
-	}
+			        /* (non-Javadoc)
+			         * @see android.support.v4.view.PagerAdapter#destroyItem(android.view.ViewGroup, int, java.lang.Object)
+			         */
+			        @Override
+			        public void destroyItem(ViewGroup container, int position, Object object) {
+			            container.removeView((View)object);
+			        }
 
-	class MyDragListener implements OnDragListener {
 
-		@Override
-		public boolean onDrag(View targetView, DragEvent event) {
-			int action = event.getAction();
-			switch (event.getAction()) {
-			case DragEvent.ACTION_DRAG_STARTED:
-				// Do nothing
-				break;
-			case DragEvent.ACTION_DRAG_ENTERED:
-				break;
-			case DragEvent.ACTION_DRAG_EXITED:
-				break;
-			case DragEvent.ACTION_DROP:
 
-				// Dropped, reassign View to ViewGroup
-				View dragged = (View) event.getLocalState();
-				ViewGroup sourceView = (ViewGroup) dragged.getParent();
+			    });
 
-				if (targetView.equals(sourceView)) {
-
-				} else {
-					sourceView.removeView(dragged);
-					LinearLayout container = (LinearLayout) targetView;
-					LayoutParams layoutParams = dragged.getLayoutParams();
-					layoutParams.height = 81;
-					layoutParams.width = 80;
-
-					RelativeLayout rel = (RelativeLayout) dragged;
-
-					int childCount = rel.getChildCount();
-					for (int i = 0; i < childCount; i++) {
-						TextView textView = (TextView) rel.getChildAt(i);
-						textView.setTextSize(15);
-						// do whatever you want to with the view
+				
+				PopoverView popoverView = new PopoverView(TheoryFragment.this.getActivity(),viewPagerLayout);
+				popoverView.setContentSizeForViewInPopover(new Point(400, 400));
+				popoverView.setDelegate(new PopoverViewDelegate() {
+					
+					@Override
+					public void popoverViewWillShow(PopoverView view) {
+						// TODO Auto-generated method stub
+						
 					}
-
 					
-					final GestureDetector gestureDetector = new GestureDetector(TheoryFragment.this.getActivity(), new GestureDetector.SimpleOnGestureListener() {
-			            @Override
-			            public boolean onDoubleTap(MotionEvent e) {
-			                Log.i("ONDOUBLE", "On double");
-			                return true;
-			            }
-			        });
+					@Override
+					public void popoverViewWillDismiss(PopoverView view) {
+						// TODO Auto-generated method stub
+						
+					}
 					
-					dragged.setOnTouchListener(new OnTouchListener() {
-					        @Override
-					        public boolean onTouch(View v, MotionEvent event) {
-					        	gestureDetector.onTouchEvent(event);
-					            return true;
-					        }
-					    });
+					@Override
+					public void popoverViewDidShow(PopoverView view) {
+						// TODO Auto-generated method stub
+						
+					}
 					
-					container.addView(dragged);
-					dragged.setVisibility(View.VISIBLE);
-				}
-				break;
-			case DragEvent.ACTION_DRAG_ENDED:
-			default:
-				break;
+					@Override
+					public void popoverViewDidDismiss(PopoverView view) {
+						// TODO Auto-generated method stub
+						
+					}
+				});
+				popoverView.showPopoverFromRectInViewGroup((ViewGroup) mainLayoutView.getParent(), PopoverView.getFrameForView(v), PopoverView.PopoverArrowDirectionAny, true);
+				
 			}
-			return true;
+		});
+		
+		
+		earthView.setOnDragListener(new TargetViewDragListener());
+		neptuneView.setOnDragListener(new TargetViewDragListener());
+		mercuryView.setOnDragListener(new TargetViewDragListener());
+		//earthView.setOnTouchListener(new MyTouchListener());
+		
+		//get all the colors and the
+		int childCount = planetColorsView.getChildCount();
+		for(int i = 0; i < childCount; i++) {
+		    View color = planetColorsView.getChildAt(i);
+		    color.setOnTouchListener(new CustomViewTouchListener());
+//		    color.setOnDragListener(new MyDragListener());
+		    System.out.println(color);
+		    // do whatever you want to with the view
 		}
+				
+		return mainLayoutView;
 	}
 
+	
+	 private final class CustomViewTouchListener implements OnTouchListener {
+		    public boolean onTouch(View view, MotionEvent event) {
+		    	
+		    	switch (event.getAction()) {
+				case MotionEvent.ACTION_DOWN:
+					ClipData data = ClipData.newPlainText("", "");
+			        DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(view);
+			        view.startDrag(data, shadowBuilder, view, 0);
+			        view.setVisibility(View.INVISIBLE);
+					break;
+
+				case MotionEvent.ACTION_UP:
+			        //view.setVisibility(View.VISIBLE);
+					break;
+				}	
+		    	return true;
+		    }
+		  }
+	 
+
+	 class TargetViewDragListener implements OnDragListener {
+		   
+		    @Override
+		    public boolean onDrag(View targetView, DragEvent event) {
+		      int action = event.getAction();
+		      switch (event.getAction()) {
+		      case DragEvent.ACTION_DRAG_STARTED:
+		        // Do nothing
+		        break;
+		      case DragEvent.ACTION_DRAG_ENTERED:
+		        break;
+		      case DragEvent.ACTION_DRAG_EXITED:
+		        break;
+		      case DragEvent.ACTION_DROP:
+		    	  
+		    	  
+		    	  
+		        // Dropped, reassign View to ViewGroup
+		        View dragged = (View) event.getLocalState();
+		        ViewGroup sourceView = (ViewGroup) dragged.getParent();
+		        
+		        if( targetView.equals(sourceView) ) {
+		        	Log.i("DRAGE", "there equal");
+		        } else {
+		        sourceView.removeView(dragged);
+		        LinearLayout container = (LinearLayout) targetView;
+		        LayoutParams layoutParams = dragged.getLayoutParams();
+		        layoutParams.height = 81;
+		        layoutParams.width = 80;
+		        
+		        RelativeLayout rel = (RelativeLayout) dragged;
+		        
+		        int childCount = rel.getChildCount();
+				for(int i = 0; i < childCount; i++) {
+				    TextView textView = (TextView) rel.getChildAt(i);
+				    textView.setTextSize(15);
+				    // do whatever you want to with the view
+				}
+		        
+				container.addView(dragged);
+				dragged.setVisibility(View.VISIBLE);
+				dragged.setOnTouchListener(null);
+				CircleView cv = (CircleView) dragged;
+				cv.initGestures();
+		        }
+		        break;
+		      case DragEvent.ACTION_DRAG_ENDED:
+		      case DragEvent.ACTION_DRAG_LOCATION:
+		    	  View v = (View) event.getLocalState();
+		          v.setVisibility(View.VISIBLE);
+		          break;
+		      default:
+		        break;
+		      }
+		      return true;
+		    }
+		  }
+	
+
+
+		
+
+	
+	
 }
--- src/ltg/heliotablet_android/view/CircleView.java
+++ src/ltg/heliotablet_android/view/CircleView.java
@@ -1,12 +1,97 @@
 package ltg.heliotablet_android.view;
 
+
+import ltg.heliotablet_android.R;
+import ltg.heliotablet_android.view.PopoverView.PopoverViewDelegate;
 import android.content.Context;
+import android.graphics.Point;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+import android.widget.RelativeLayout;
 
-public class CircleView extends View {
+public class CircleView extends RelativeLayout implements PopoverViewDelegate  {
 
+	private GestureDetector gestureDetector;
+	
 	public CircleView(Context context) {
 		super(context);
+		init();
+	}
+	
+	public CircleView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+	private void init() {
+	}
+ 
+	public void initGestures() {
+		
+		
+		gestureDetector = new GestureDetector(getContext(), new GestureDetector.SimpleOnGestureListener(){
+			   
+			   @Override
+			public boolean onDoubleTap(MotionEvent e) {
+				   Log.i("CLICK", "double tap event DOUBLE TAP");
+				   float x = e.getX();
+			        float y = e.getY();
+
+			        Log.i("Double Tap", "Tapped at: (" + x + "," + y + ")");
+
+			        ViewParent parent = CircleView.this.getParent().getParent().getParent();
+			        
+			        
+//			        /RelativeLayout rootView = (RelativeLayout)findViewById(R.id.rootLayout);
+					
+					PopoverView popoverView = new PopoverView(CircleView.this.getContext(), R.layout.activity_screen_slide);
+					popoverView.setContentSizeForViewInPopover(new Point(500, 340));
+					popoverView.setDelegate(CircleView.this);
+					popoverView.showPopoverFromRectInViewGroup((ViewGroup) parent, PopoverView.getFrameForView(CircleView.this), PopoverView.PopoverArrowDirectionAny, true);
+			        
+			        
+
+				   
+				return super.onDoubleTap(e);
+			}
+		   });
+		
+		this.setOnTouchListener(new OnTouchListener() {
+			
+			@Override
+			public boolean onTouch(View v, MotionEvent event) {
+				gestureDetector.onTouchEvent(event);
+				return true;
+			}
+		});
+	}
+	
+	@Override
+	public void popoverViewWillShow(PopoverView view) {
+		// TODO Auto-generated method stub
+		
 	}
 
+	@Override
+	public void popoverViewDidShow(PopoverView view) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void popoverViewWillDismiss(PopoverView view) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void popoverViewDidDismiss(PopoverView view) {
+		// TODO Auto-generated method stub
+		
+	}
 }
