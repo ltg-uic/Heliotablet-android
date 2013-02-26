57b4094c9cb5e19249c1c2a8c1aac128079e7e72
Anthony Perritano <aperri3@uic.edu>
2013-02-25 23:00:08 -0600

    observation popover working

--- src/ltg/heliotablet_android/view/ObservationCircleView.java
+++ src/ltg/heliotablet_android/view/ObservationCircleView.java
@@ -1,27 +1,43 @@
 package ltg.heliotablet_android.view;
 
-import com.google.common.collect.ImmutableSortedSet;
+import java.util.ArrayList;
 
 import ltg.heliotablet_android.R;
 import ltg.heliotablet_android.R.color;
 import ltg.heliotablet_android.data.Reason;
+import ltg.heliotablet_android.view.PopoverView.PopoverViewDelegate;
 import android.content.Context;
+import android.content.res.Resources;
 import android.content.res.TypedArray;
+import android.graphics.Point;
 import android.graphics.drawable.Drawable;
+import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
+import android.view.GestureDetector;
 import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class ObservationCircleView extends RelativeLayout implements ICircleView {
+import com.google.common.collect.ImmutableSortedSet;
+
+public class ObservationCircleView extends RelativeLayout implements ICircleView, PopoverViewDelegate {
 
 	private String flag;
 	private int textColor;
 	private TextView reasonTextView;
 	private ImmutableSortedSet<Reason> imReasons;
 	private String anchor;
+	private GestureDetector gestureDetector;
+
+	private PopoverView cachedPopoverView;
+	private RelativeLayout viewPagerLayout;
+	private Reason reasonNeedsUpdate;
 	
 	public ObservationCircleView(Context context) {
 		super(context);
@@ -40,10 +56,98 @@ public class ObservationCircleView extends RelativeLayout implements ICircleView
         this.setTextColor(a.getColor(R.styleable.CircleView_textColor, color.White));
         a.recycle();
         
-       // this.setLayoutParams(new RelativeLayout.LayoutParams(77,77));
+        viewPagerLayout = (RelativeLayout) inflater.inflate(
+				R.layout.activity_screen_slide, this, false);
         reasonTextView.setTextColor(this.getTextColor());
+        this.enableDoubleTap();
     }
 	
+	
+	public void showPopover(ImmutableSortedSet<Reason> popOverReasonSet) {
+		Resources resources = getResources();
+		ArrayList<View> pages = new ArrayList<View>();
+		ViewPager pager = new ViewPager(getContext());
+		ViewPager vPager = (ViewPager) viewPagerLayout
+				.findViewById(R.id.pager);
+		
+		for (Reason reason : popOverReasonSet) {
+			View layout = View.inflate(getContext(),
+					R.layout.popover_view_obs_choose_delete, null);
+			layout.setTag(reason);
+			pages.add(layout);
+			
+			
+			TextView reasonText = (TextView) layout.findViewById(R.id.reasonTitle);
+			String text = String.format(resources.getString(R.string.obs_reason_text), reason.getFlag(), reason.getAnchor());
+			reasonText.setText(text);
+			
+			int i = 2;
+			Button reasonButton1 = (Button) layout.findViewById(R.id.reasonButton1);
+			text = String.format(resources.getString(R.string.obs_reason_button_1), reason.getFlag(), reason.getAnchor(),i);
+			reasonButton1.setText(text);
+			
+			Button reasonButton2 = (Button) layout.findViewById(R.id.reasonButton2);
+			text = String.format(resources.getString(R.string.obs_reason_button_2), reason.getFlag(), reason.getAnchor(),i);
+			reasonButton2.setText(text);
+			
+			Button reasonButton3 = (Button) layout.findViewById(R.id.reasonButton3);
+			text = String.format(resources.getString(R.string.obs_reason_button_3),i);
+			reasonButton3.setText(text);
+			
+			
+			
+			Button deleteButton = (Button) layout.findViewById(R.id.deleteButton);
+			
+				deleteButton.setVisibility(View.VISIBLE);
+				deleteButton.setOnClickListener(new OnClickListener() {
+					
+					@Override
+					public void onClick(View v) {
+						
+						
+						
+					}
+				});
+		}
+		
+		vPager.setAdapter(new PopoverViewAdapter(pages));
+		vPager.setOffscreenPageLimit(5);
+		vPager.setCurrentItem(0);
+        ViewGroup parent = (ViewGroup) this.getParent().getParent().getParent();
+
+		
+		PopoverView popoverView = new PopoverView(getContext(), viewPagerLayout);
+		cachedPopoverView = popoverView;
+		popoverView.setContentSizeForViewInPopover(new Point(520, 220));
+		popoverView.setDelegate(this);
+		popoverView.showPopoverFromRectInViewGroup(
+				parent,
+				PopoverView.getFrameForView(this),
+				PopoverView.PopoverArrowDirectionAny, true);
+		
+	}
+	
+	@Override
+	public void popoverViewWillShow(PopoverView view) {
+	}
+
+	@Override
+	public void popoverViewDidShow(PopoverView view) {
+	
+	}
+
+	public void popoverViewWillDismiss(PopoverView view) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public void popoverViewDidDismiss(PopoverView view) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
 	public int getReasonsSize() {
 		if( imReasons != null ){
 			imReasons.size();
@@ -123,6 +227,33 @@ public class ObservationCircleView extends RelativeLayout implements ICircleView
 	public void setAnchor(String anchor) {
 		this.anchor = anchor;
 	}
+	
+	public void enableDoubleTap() {
+        gestureDetector = new GestureDetector(getContext(), new CircleDoubleTapGestureListener());
+        this.setOnTouchListener(new OnTouchListener() {
+			
+			@Override
+			public boolean onTouch(View v, MotionEvent event) {
+				 gestureDetector.onTouchEvent(event);
+				 return true;
+			}
+		}); 
+	}
+	
+	class CircleDoubleTapGestureListener extends GestureDetector.SimpleOnGestureListener {
+	    // event when double tap occurs
+	    @Override
+	    public boolean onDoubleTap(MotionEvent e) {
+	    	
+	    	showPopover(imReasons);
+	        return super.onDoubleTap(e);
+	    }
+	}
+
+
+	
+
+	
 
 
 }
